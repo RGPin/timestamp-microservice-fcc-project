cb72a0f169cc18eb68dc25e5e39c40873944b396
RGPin <pinosanrg@gmail.com>
2025-04-27 05:01:12 +0800

    here my solution

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { isNull } = require('mathjs');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -23,7 +24,28 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+// sample requests: /api/2015-12-25, /api/1451001600000
+app.get("/api/:date", (req, res) => {
+  let dateObject;
+  const digitsOnlyCheck = /^\d+$/;
+  if(digitsOnlyCheck.test(req.params.date)) {
+    const timeStampUnix = Number(req.params.date);
+    dateObject = new Date(timeStampUnix);
+  } else {
+    dateObject = new Date(req.params.date);
+  }
+  if(isNaN(dateObject.getTime())) {
+    res.json({"error": "Invalid Date"});
+  } else {
+    res.json({"unix": dateObject.getTime(), "utc": dateObject.toUTCString()});
+  }
+    
+});
 
+app.get("/api/", (req, res) => {
+  const dateObject = new Date();
+  res.json({"unix": dateObject.getTime(), "utc": dateObject.toUTCString()});
+});
 
 
 // Listen on port set in environment variable or default to 3000
